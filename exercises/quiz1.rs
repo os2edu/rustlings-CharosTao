0f011e97d6b30119898445bd288f19363e864bcf
taodaili <taodaili@taodailideMacBook-Pro.local>
2023-03-31 00:33:18 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(size: u32) -> u32 {
+    if size > 40 {
+        size
+    } else {
+        size * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
